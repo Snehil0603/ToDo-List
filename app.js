dbc87431a5511aadd6e9c04cfb94fad72703aa39
unknown <samayragupta7thapril@gmail.com>
2024-11-05 14:57:00 +0530

    udating connection

--- app.js
+++ app.js
@@ -20,8 +20,9 @@ app.set('view engine', 'ejs');
 
 // Connecting to mongodb cluster
 
-mongoose.connect(process.env.MONGO_URI);
-
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(error => console.error('MongoDB connection error:', error));
 
 // Making a schema for our collection
 const newItemSchema=new mongoose.Schema({
